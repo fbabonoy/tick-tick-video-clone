16e1c19760b2f99b8dd1501f712fc06c1082809a
Fernando babonoyaba <fernando.babonoyaba@gmail.com>
2022-08-05 18:13:12 -0400

    added collevtion view, video player, and working on layour know

---
+++ videoPlayer/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  videoPlayer
+//
+//  Created by fernando babonoyaba on 7/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    lazy var carousel: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.frame.size.width, height: view.frame.size.height)
+        layout.footerReferenceSize = .zero
+        layout.headerReferenceSize = .zero
+        let videoCollection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        videoCollection.isPagingEnabled = true
+        videoCollection.translatesAutoresizingMaskIntoConstraints = false
+        videoCollection.dataSource = self
+        videoCollection.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.videoCell)
+        videoCollection.backgroundColor = .white
+        return videoCollection
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        carousel.frame = view.bounds
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        view.addSubview(carousel)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.videoCell, for: indexPath) as! CollectionViewCell
+        
+        return cell
+    }
+
+}
+
